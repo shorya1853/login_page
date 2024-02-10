c2b78c4fd6658cde1efa2ab29d3b6660a0c49daa
Shorya Dubey <98734613+shorya1853@users.noreply.github.com>
2024-02-10 22:55:40 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:login_ui/screens/login_screen.dart';
 
-import 'screens/register.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meals/screens/categories.dart';
 
-void main(){
-  runApp( const MyApp());
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+void main() {
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
-  Widget build(context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      theme: theme,
+      home: const CategoriesScreen(),
     );
   }
 }
